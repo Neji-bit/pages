526b8b788cba6c3a3bf2588d7bd4b388a697c4a8
Neji <85750466+Neji-bit@users.noreply.github.com>
2023-05-04 22:17:58 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+let cacheName = 'logbo'
+let filesToCache = [
+  '/',
+]
+
+self.addEventListener('install', (e) => {                              
+  console.log('ServiceWorker installing...')                           
+  e.waitUntil(                                                         
+    caches.open(cacheName).then((c) => {                               
+      console.log('Service Worker caching app shell')                  
+      return c.addAll(filesToCache)                                    
+    })                                                                 
+  ) 
+})
